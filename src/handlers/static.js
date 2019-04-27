db08e67888d00e0c2f4238cd3ab0991bb024732a
Nicolas Dao <nicolas.dao@gmail.com>
2019-04-27 10:27:04 +1000

    feat: Method:'staticHandler'. Add support for defining default page when URI such as https//neap.co are specified

--- src/handlers/static.js
+++ src/handlers/static.js
@@ -1,20 +1,34 @@
 const co = require('co')
 const { static:expressStatic } = require('express')
-const { resolve } = require('path')
+const { resolve, basename, extname } = require('path')
 const { files } = require('../utils')
+const fs = require('fs')
 
-const staticHandler = (...args) => {
-	const folder = args[0]
-	let handler = expressStatic(...args)
+const staticHandler = (folder, options) => {
+	if (!folder)
+		throw new Error('Missing required argument \'folder\'.')
+	const { defaultPage='index.html' } = options || {}
+	const defaultPageValid = defaultPage && extname(defaultPage)
+	let handler = expressStatic(folder)
 	handler.type = 'static'
 	handler.config = app => co(function *() {
 		const folderPath = resolve(folder)
 		const _files = (yield files.get(folderPath, { pattern:'**/*.*' })) || []
-		_files.forEach(f => app.get(f.replace(folderPath,''),handler))
+		_files.forEach(f => {
+			const relPath = f.replace(folderPath,'')
+			if (defaultPageValid && basename(f) == defaultPage) {
+				const aliasPath = relPath.split('/').slice(0,-1).join('/')
+				app.get(aliasPath || '/', (req,res) => fs.readFile(f, (err,data) => {
+					if (data)
+						res.status(200).send(data.toString())
+					else
+						res.status(404).send('File not found')
+				}))
+			}
+			app.get(relPath, handler)
+		})
 	})
 	return handler
 }
 
 module.exports = staticHandler
-
-
